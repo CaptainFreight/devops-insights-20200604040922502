ce96e441b004a63044cc9f566a0d109d5397e1d8
shaan nagra <h.s.nagra98@gmail.com>
2020-06-05 00:50:50 +0000

    made routes apiv1.js to work with nz city names

--- routes/apiv1.js
+++ routes/apiv1.js
@@ -10,12 +10,12 @@ var request = REQUEST.defaults( {
 var OPENWEATHERURL = "https://api.openweathermap.org/data/2.5/weather?appid=6b7b471967dd0851d0010cdecf28f829&units=metrics";
 
 exports.getWeather = function(req, res) {
-	var zip = req.query.zip;
-	if( (zip === null) || (typeof(zip) === 'undefined') ) {
-		return res.status(400).send('zip missing');
+	var name = req.query.name;
+	if( (name === null) || (typeof(name) === 'undefined') ) {
+		return res.status(400).send('name missing');
 	}
 
-	var aurl = OPENWEATHERURL + '&zip=' + zip + ',us';
+	var aurl = OPENWEATHERURL + '&q=' + name + ',nz';
 
 	request({
 		method: 'GET',
@@ -41,7 +41,7 @@ router.get('/getWeather', exports.getWeather);
 
 /*
 exports.getWeather2 = function(req, res) {
-	var zip = req.query.zip;
+	var zip = req.query.name;
 	if( (zip === null) || (typeof(zip) === 'undefined') ) {
 		return res.status(400).send('zip missing');
 	}
@@ -71,12 +71,12 @@ exports.getWeather2 = function(req, res) {
 router.get('/getWeather2', exports.getWeather2);
 */
 exports.getWeather3 = function(req, res) {
-	var zip = req.query.zip;
+	var zip = req.query.name;
 	if( (zip === null) || (typeof(zip) === 'undefined') ) {
-		return res.status(400).send('zip missing');
+		return res.status(400).send('name missing');
 	}
 
-	var aurl = OPENWEATHERURL + '&zip=' + zip + ',us';
+	var aurl = OPENWEATHERURL + '&q=' + zip + ',nz';
 
 	request({
 		method: 'GET',
